59333a3a04084f72c2c9e75f4435b6e7a300d9dc
AESalva <eduardo_salva@hotmail.com>
2022-08-27 17:03:20 -0300

    changes and add second game

--- javascript/main.js
+++ javascript/main.js
@@ -1,40 +1,67 @@
-const DATA_GAMES= [
-    {
-        id:"diablo",
-        name:"Diablo III & Reaper of Souls",
-        titulo:"RESPONDE LA LLAMADA A LA BATALLA",
-        historia:"Lucha contra males ancestrales y desvela la historia de los sucesos transcurridos entre Diablo II y Diablo III en este nuevo RPG de acción multijugador masivo en línea desarrollado para móvil pero con beta abierta también disponible próximamente en PC",
-        resumen:"   Al arcángel Tyrael se lo da por muerto, y la humanidad está sola para afrontar las consecuencias de sus actos Los fragmentos de la Piedra del Mundo aún dotados de un inmenso poder, contaminan la tierra y los esbirros de Diablo esperan poder controlar ese poder para que el Señor del Terror pueda regresar",
-        subtitulo:"MATA DEMONIOS DONDEQUIERA QUE ESTÉS",
-        img_historia:"/RollingGames/AssetData/Diablo3/bg_historia/249110.jpg",
-        img_muestra:[
-            "/RollingGames/AssetData/Diablo3/Carrousel/diablo-3-reaper-of-souls-ultimate-evil-edition-rev_z2rq.1200.webp",
-            "/RollingGames/AssetData/Diablo3/Carrousel/Diablo-III.webp",
-            "/RollingGames/AssetData/Diablo3/Carrousel/diablo3.webp",
-        ],
-        imagenes:[
-            "/RollingGames/AssetData/Diablo3/trailer/D3_LH_107_072.jpg",
-            "/RollingGames/AssetData/Diablo3/trailer/diablo-3-patch-2-1-out-this-week-1409044556927.jpg",
-            "/RollingGames/AssetData/Diablo3/trailer/maxresdefault.jpg",
-        ],
-        animation:[
-            "/RollingGames/AssetData/Diablo3/trailer/diablo1.gif",
-            "/RollingGames/AssetData/Diablo3/trailer/diablo2.gif",
-            "/RollingGames/AssetData/Diablo3/trailer/diablo3.gif",
-        ]
-    }
+const DATA_GAMES = [
+  {
+    id: "diablo",
+    name: "Diablo III & Reaper of Souls",
+    titulo: "RESPONDE LA LLAMADA A LA BATALLA",
+    historia:
+      "Lucha contra males ancestrales y desvela la historia de los sucesos transcurridos entre Diablo II y Diablo III en este nuevo RPG de acción multijugador masivo en línea desarrollado para móvil pero con beta abierta también disponible próximamente en PC",
+    resumen:
+      "   Al arcángel Tyrael se lo da por muerto, y la humanidad está sola para afrontar las consecuencias de sus actos Los fragmentos de la Piedra del Mundo aún dotados de un inmenso poder, contaminan la tierra y los esbirros de Diablo esperan poder controlar ese poder para que el Señor del Terror pueda regresar",
+    subtitulo: "MATA DEMONIOS DONDEQUIERA QUE ESTÉS",
+    img_historia: "/RollingGames/AssetData/Diablo3/bg_historia/249110.jpg",
+    img_muestra: [
+      "/RollingGames/AssetData/Diablo3/Carrousel/diablo-3-reaper-of-souls-ultimate-evil-edition-rev_z2rq.1200.webp",
+      "/RollingGames/AssetData/Diablo3/Carrousel/Diablo-III.webp",
+      "/RollingGames/AssetData/Diablo3/Carrousel/diablo3.webp",
+    ],
+    imagenes: [
+      "/RollingGames/AssetData/Diablo3/trailer/D3_LH_107_072.jpg",
+      "/RollingGames/AssetData/Diablo3/trailer/diablo-3-patch-2-1-out-this-week-1409044556927.jpg",
+      "/RollingGames/AssetData/Diablo3/trailer/maxresdefault.jpg",
+    ],
+    animation: [
+      "/RollingGames/AssetData/Diablo3/trailer/diablo1.gif",
+      "/RollingGames/AssetData/Diablo3/trailer/diablo2.gif",
+      "/RollingGames/AssetData/Diablo3/trailer/diablo3.gif",
+    ],
+  },
+  {
+    id: "hearthstone",
+    name: "HearthStone",
+    titulo: "Colecciona cartas y arma mazos",
+    historia:
+      "Las cartas de Hearthstone, ya sean esbirros, hechizos, misiones o héroes, pueden cambiar drásticamente el curso de la partida con efectos poderosos e interacciones alocadas. ¡No te distraigas y supera a tu enemigo con tu ingenio!",
+    resumen:
+      "Todos pueden jugar Hearthstone. Practica tus habilidades en partidas casuales, sube de rango en el modo clasificado, disfruta de un giro divertido en las Grescas de la taberna, pon a prueba tu suerte en la Arena, juega aventuras en solitario contra adversarios malignos e ingeniosos controlados por la computadora y mucho, mucho más",
+    subtitulo: "Juega a tu estilo",
+    img_historia:
+      "/RollingGames/AssetData/hearthstone/bg_historia/A12Uzob0MJL.jpg",
+    img_muestra: [
+      "/RollingGames/AssetData/hearthstone/Carrousel/Como-jugar-Hearthstone-en-Linux.jpg",
+      "/RollingGames/AssetData/hearthstone/Carrousel/default-4fff3c606c794dc03a915b9071f562d3.jpg",
+      "/RollingGames/AssetData/hearthstone/Carrousel/og_image-2e1ee3c5546d60c597788e9d2852cfca86b2322dd574acf7fe4d322d94ead7097cd6a021c716b44d572711b840bf1f106a300fbaebce3a020554c177a918ee6b.jpg",
+    ],
+    imagenes: [
+      "/RollingGames/AssetData/hearthstone/trailer/maxresdefault (2).jpg",
+      "/RollingGames/AssetData/hearthstone/trailer/maxresdefault (1).jpg",
+      "/RollingGames/AssetData/hearthstone/trailer/maxresdefault.jpg",
+    ],
+    animation: [
+      "/RollingGames/AssetData/hearthstone/trailer/img1_juego.gif",
+      "/RollingGames/AssetData/hearthstone/trailer/img2_juego.gif",
+      "/RollingGames/AssetData/hearthstone/trailer/img3_juego.gif",
+    ],
+  },
 ];
 
-
-const conteiner = document.getElementById('conteiner');
-
+const conteiner = document.getElementById("conteiner");
 
 function createPage(data, id) {
-    data.map(d=>{
-        const index = DATA_GAMES.indexOf(d);
-        if (id === DATA_GAMES[index].id) {
-        const section = document.createElement('section');
-        section.innerHTML = `<div
+  data.map((d) => {
+    const index = DATA_GAMES.indexOf(d);
+    if (id === DATA_GAMES[index].id) {
+      const section = document.createElement("section");
+      section.innerHTML = `<div
         id="carouselExampleControlsNoTouching"
         class="carousel slide px-5"
         data-bs-touch="false"
@@ -87,9 +114,9 @@ function createPage(data, id) {
           <span class="visually-hidden">Next</span>
         </button>
       </div>`;
-        conteiner.appendChild(section);
-        const section2 = document.createElement('section');
-        section2.innerHTML = `<h1 class="juego_titulo p-5">${d.titulo}</h1>
+      conteiner.appendChild(section);
+      const section2 = document.createElement("section");
+      section2.innerHTML = `<h1 class="juego_titulo p-5">${d.titulo}</h1>
         <article class="historia">
           <img
             class="bg_img_historia"
@@ -100,9 +127,9 @@ function createPage(data, id) {
             ${d.historia}
           </p>
         </article>`;
-        conteiner.appendChild(section2);
-        const section3 = document.createElement('section');
-        section3.innerHTML = `  <p class="resumen_juegp">
+      conteiner.appendChild(section2);
+      const section3 = document.createElement("section");
+      section3.innerHTML = `  <p class="resumen_juegp">
         ${d.resumen}
       </p>
       <h2 class="juego_titulo p-5">${d.subtitulo}</h2>
@@ -143,13 +170,9 @@ function createPage(data, id) {
       </div>`;
 
       conteiner.appendChild(section3);
-        }
-        document.title = DATA_GAMES[index].name;
-    });
+      document.title = DATA_GAMES[index].name;
+    }
+  });
 }
 
-createPage(DATA_GAMES,"diablo");
-
-
-
-
+createPage(DATA_GAMES, "diablo");
